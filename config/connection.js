0b0babff83c9eb2e6c3207874683b4c30564cd45
KDarch928 <Kayle.Drucker@gmail.com>
2018-03-15 21:56:20 -0500

    added JAWSDB_URL to connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,28 +4,28 @@ var mysql = require("mysql");
 require("dotenv").config();
 //getting access to the key.js file
 var key = require("./key.js");
-// var connection;
-//
-// if (process.env.JAWSDB_URL){
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//     connection = mysql.createConnection({
-//         port: 3306,
-//         host: "localhost",
-//         user: "root",
-//         password: key.mysqlaccess.access,
-//         database: "burgers_db"
-//     });
-// };
+var connection;
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: key.mysqlaccess.access,
+        database: "burgers_db"
+    });
+};
 
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: key.mysqlaccess.access,
-    database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//     port: 3306,
+//     host: "localhost",
+//     user: "root",
+//     password: key.mysqlaccess.access,
+//     database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
